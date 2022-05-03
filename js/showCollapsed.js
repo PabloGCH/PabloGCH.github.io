7e28022704c515880ebeac2f145b37d3993e9f39
PabloGCH <pablochoconi@gmail.com>
2022-05-02 23:30:48 -0300

    Todos los links funcionan correctamente

--- js/showCollapsed.js
+++ js/showCollapsed.js
@@ -1,21 +1,24 @@
 var sidebar = document.getElementById("sidebar");
 var sidebarLinkCollection = document.getElementsByClassName("sidebar-nav-link");
 var sidebarLinks = Array.from(sidebarLinkCollection);
+var bootstrapCollapseSidebar = new bootstrap.Collapse(sidebar, {
 
-//Se asegura que los links tengan los atributos correctos para colapsar el sidebar
+});
+
+function sidebarToggle() {
+    bootstrapCollapseSidebar.toggle();
+}
+
+
 function setLinks() {
     sidebarLinks.forEach(link => {
-        link.setAttribute("data-bs-toggle","collapse");
-        link.setAttribute("data-bs-target","#sidebar");
-        link.setAttribute("aria-expanded","true");
+        link.addEventListener('click', sidebarToggle)
     });
 }
-//Se asegura que los links no puedan colapsar el sidebar
+
 function unsetLinks() {
     sidebarLinks.forEach(link => {
-        link.removeAttribute("data-bs-toggle");
-        link.removeAttribute("data-bs-target");
-        link.removeAttribute("aria-expanded");
+        link.removeEventListener('click', sidebarToggle);
     });
 }
 
@@ -29,7 +32,6 @@ function showCollapsed(x) {
         sidebar.classList.remove("show");
     }
 }
-
 var x = window.matchMedia("(min-width: 768px)");
 showCollapsed(x);
 x.addEventListener("change", () => {
